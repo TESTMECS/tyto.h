080c827a1808e957c6f8281208ace7aef1524e1a
TESTMECS <axwar7410@gmail.com>
2026-06-12 16:58:24 -0400

    todo tests

--- test.c
+++ test.c
@@ -19,22 +19,11 @@ test_slice(void)
     xassert("Should check empty", slice_is_empty(slice_cstring("")));
 }
 
-void
-test_arena(void)
-{
-    Arena* a = arena_new();
-    char*  x = arena_alloc(a, 10);
-    char*  y = arena_alloc(a, 10);
-    char*  z = arena_alloc(a, 10);
-    arena_free(a);
-}
-
 int
 main(void)
 {
     puts("=== Begin Tests ===");
     test_slice();
-    test_arena();
     puts("=== Tests passed! ===");
     return 0;
 }
